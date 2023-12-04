836315e9b1b06b6c0d76921105ba93ce78dcba32
Mustafa <mostafakhaled0787314@gmail.com>
2023-12-04 11:07:19 +0200

    implement routes

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Pizza</Link>
+
+      <p>Mustafa</p>
+    </header>
+  );
+}
+
+export default Header;
