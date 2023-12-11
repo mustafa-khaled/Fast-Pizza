600ad7ab13e6d91103fa6c9169eb5a536dec5937
Mustafa <mostafakhaled0787314@gmail.com>
2023-12-11 13:31:37 +0200

    swiper responsiveness

--- src/ui/home/PizzaSwiper.jsx
+++ src/ui/home/PizzaSwiper.jsx
@@ -6,6 +6,7 @@ import "swiper/css/navigation";
 import "swiper/css";
 
 import MenuItem from "../../features/menu/MenuItem";
+import Container from "../Container";
 
 function PizzaSwiper() {
   const fetcher = useFetcher();
@@ -18,39 +19,54 @@ function PizzaSwiper() {
     return <p className="text-center text-2xl uppercase">Loading...</p>;
 
   return (
-    <Swiper
-      // className="w-full"
-      className=" grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] sm:grid-cols-[repeat(auto-fill,minmax(270px,1fr))]"
-      modules={[Navigation, Pagination, Scrollbar]}
-      spaceBetween={50}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      breakpoints={{
-        400: {
-          slidesPerView: 1,
-        },
-        500: {
-          slidesPerView: 2,
-        },
+    <Container>
+      <div>
+        <div className="mb-[30px] text-center font-bold">
+          <p className="text-sm uppercase text-primary">Choose Your Flavor</p>
+          <h2 className="my-[5px] text-xl font-bold uppercase sm:text-3xl">
+            The Best Pizza Menu In Town
+          </h2>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit
+            arcu in pretium molestie. Interdum et malesuada fames ac.
+          </p>
+        </div>
 
-        900: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 5,
-        },
-      }}
-    >
-      {fetcher?.data?.length > 0 &&
-        fetcher?.data?.slice(0, 7)?.map((item) => {
-          return (
-            <SwiperSlide key={item.id}>
-              <MenuItem pizza={item} key={item.id} />
-            </SwiperSlide>
-          );
-        })}
-    </Swiper>
+        <Swiper
+          // className="w-full"
+          className=" grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] sm:grid-cols-[repeat(auto-fill,minmax(270px,1fr))]"
+          modules={[Navigation, Pagination, Scrollbar]}
+          spaceBetween={50}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          breakpoints={{
+            400: {
+              slidesPerView: 1,
+            },
+            500: {
+              slidesPerView: 2,
+            },
+
+            900: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {fetcher?.data?.length > 0 &&
+            fetcher?.data?.slice(0, 7)?.map((item) => {
+              return (
+                <SwiperSlide key={item.id}>
+                  <MenuItem pizza={item} key={item.id} />
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+      </div>
+    </Container>
   );
 }
 
